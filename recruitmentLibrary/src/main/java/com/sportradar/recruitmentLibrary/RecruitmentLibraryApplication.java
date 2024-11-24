109b728504e2ba4809daae212f7a209323ed45c0
Vontariel <vontariel@gmail.com>
2024-11-24 14:37:42 +0100

    [RecruitmentTask] Feature: Establishing git repository & barebones project (spring Initialzr)

---
+++ recruitmentLibrary/src/main/java/com/sportradar/recruitmentLibrary/RecruitmentLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.recruitmentLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentLibraryApplication.class, args);
+	}
+
+}
---
+++ recruitmentLibrary/src/test/java/com/sportradar/recruitmentLibrary/RecruitmentLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.recruitmentLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
