bce13138ea81622322a384b7aa961bf0136fdd39
Vontariel <vontariel@gmail.com>
2024-11-24 23:49:16 +0100

    [RecruitmentTask] Bugfix: Typos, asterisk import, changing javadoc for Scoreboard

--- recruitmentLibrary/src/main/java/com/sportradar/recruitmentLibrary/Scoreboard.java
+++ recruitmentLibrary/src/main/java/com/sportradar/recruitmentLibrary/Scoreboard.java
@@ -5,19 +5,10 @@ import java.util.*;
 import static java.util.Comparator.reverseOrder;
 
 /**
- * You are working in a sports data company, and we would like you to develop a new Live Football
- * World Cup Scoreboard library that shows all the ongoing matches and their scores.
- * The scoreboard supports the following operations:
- * 1. Start a new match, assuming initial score 0 – 0 and adding it the scoreboard.
- * This should capture following parameters:
- * a. Home team
- * b. Away team
- * 2. Update score. This should receive a pair of absolute scores: home team score and away
- * team score.
- * 3. Finish match currently in progress. This removes a match from the scoreboard.
- * 4. Get a summary of matches in progress ordered by their total score. The matches with the
- * same total score will be returned ordered by the most recently started match in the
- * scoreboard.
+ * Simple implementation of Scoreboard functionality for active games tracking.
+ *
+ * @author Robert Radzimowski
+ * @version 1.0.0
  */
 public class Scoreboard {
     private final Map<String, Match> ongoingMatches = new HashMap<>();
@@ -76,7 +67,7 @@ public class Scoreboard {
     }
 
     /**
-     * @return sorted list based on score (higher up) then creationDate (earlier up)
+     * @return sorted list based on score (higher up) then creationDate (newer up)
      */
     public List<Match> getSummary() {
         Collection<Match> summary = ongoingMatches.values();
